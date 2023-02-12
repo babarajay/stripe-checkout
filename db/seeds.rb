107864a029f739b6ad4793c5e267df746714659c
Maulik Bafalipara <maulikbafalipara007@gmail.com>
2023-02-12 10:25:41 +0530

    seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Product.create([{
   title: Faker::Commerce.product_name[0..20],
   description: Faker::Lorem.sentence(word_count: 15),
   unit_amount: Faker::Commerce.price(range: 1000..10000.0, as_string: true),
-  image_url: "https://picsum.photos/200/300",
+  image_url: "https://picsum.photos/200",
 }])
 end
 
