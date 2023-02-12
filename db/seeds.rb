51e355b8474401b60868b83961a2c3dd51115e8d
Macbook <macbook@192.168.0.104>
2023-02-12 10:23:34 +0530

    bootstrap and design changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Product.destroy_all
-20.times do 
+10.times do
 Product.create([{
   title: Faker::Commerce.product_name[0..20],
   description: Faker::Lorem.sentence(word_count: 15),
   unit_amount: Faker::Commerce.price(range: 1000..10000.0, as_string: true),
-  image_url: "https://ucarecdn.com/93d2f6b9-596c-46d1-be59-c8335d22bdd4/fancypants.jpeg",
+  image_url: "https://picsum.photos/200/300",
 }])
 end
 
